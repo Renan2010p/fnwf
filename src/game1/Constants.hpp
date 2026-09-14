2584d7b5b699ef650dc06ad5f9656a6bac2647da
Renan2010p <renanluscad@proton.me>
2026-09-14 14:26:26 -0700

    Fix PS2 build: use int instead of int32_t for screen constants (int32_t=long on mips64)

--- src/game1/Constants.hpp
+++ src/game1/Constants.hpp
@@ -4,17 +4,17 @@
 
 namespace fnwf::GameSettings {
 
-constexpr std::int32_t SCREEN_WIDTH = 1280;
-constexpr std::int32_t SCREEN_HEIGHT = 720;
-constexpr std::int32_t FPS = 60;
+constexpr int SCREEN_WIDTH = 1280;
+constexpr int SCREEN_HEIGHT = 720;
+constexpr int FPS = 60;
 constexpr const char* ASSETS_DIR = "assets";
 
 constexpr double HOUR_DURATION = 60.0;
 constexpr double MAX_POWER = 100.0;
 constexpr double BASE_POWER_DRAIN = 0.12;
-constexpr std::int32_t OFFICE_WIDTH = 1280;
+constexpr int OFFICE_WIDTH = 1280;
 constexpr double PAN_SPEED = 8.0;
-constexpr std::int32_t PAN_MARGIN = 200;
+constexpr int PAN_MARGIN = 200;
 constexpr double MOVE_INTERVAL = 5.0;
 
 }  // namespace fnwf::GameSettings
