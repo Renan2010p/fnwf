984e76f7dffe7a427645f6aadbdf4f66ee0746c1
Renan2010p <renanluscad@proton.me>
2026-09-15 14:56:05 -0700

    PS2: use backslash paths for cdrom0 device

--- src/game1/Constants.hpp
+++ src/game1/Constants.hpp
@@ -10,7 +10,7 @@ namespace fnwf::GameSettings {
 constexpr int SCREEN_WIDTH = 640;
 constexpr int SCREEN_HEIGHT = 448;
 constexpr int FPS = 60;
-constexpr const char* ASSETS_DIR = "CDROM0:/ASSETS";
+constexpr const char* ASSETS_DIR = "CDROM0:\\ASSETS";
 
 constexpr double HOUR_DURATION = 60.0;
 constexpr double MAX_POWER = 100.0;
@@ -22,7 +22,7 @@ inline std::string asset_path(const std::string& sub, const std::string& ext) {
     std::string upper_ext = ext;
     std::transform(upper_sub.begin(), upper_sub.end(), upper_sub.begin(), ::toupper);
     std::transform(upper_ext.begin(), upper_ext.end(), upper_ext.begin(), ::toupper);
-    return std::string(ASSETS_DIR) + "/" + upper_sub + upper_ext;
+    return std::string(ASSETS_DIR) + "\\" + upper_sub + upper_ext;
 }
 
 inline std::string asset_file(const std::string& name, const std::string& ext) {
@@ -30,13 +30,13 @@ inline std::string asset_file(const std::string& name, const std::string& ext) {
     std::string upper_ext = ext;
     std::transform(upper_name.begin(), upper_name.end(), upper_name.begin(), ::toupper);
     std::transform(upper_ext.begin(), upper_ext.end(), upper_ext.begin(), ::toupper);
-    return std::string(ASSETS_DIR) + "/" + upper_name + upper_ext;
+    return std::string(ASSETS_DIR) + "\\" + upper_name + upper_ext;
 }
 
 inline std::string asset_full(const std::string& rel) {
     std::string upper = rel;
     std::transform(upper.begin(), upper.end(), upper.begin(), ::toupper);
-    return std::string(ASSETS_DIR) + "/" + upper;
+    return std::string(ASSETS_DIR) + "\\" + upper;
 }
 #else
 constexpr int SCREEN_WIDTH = 1280;
