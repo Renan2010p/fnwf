2b2443b04ead7f81ab077b0bc34c96bf16dc8c40
Renan2010p <renanluscad@proton.me>
2026-09-21 18:28:51 -0300

    fix: add -mno-abicalls -fno-pic to fix GCC MIPS EABI conflict

--- build-ps2.sh
+++ build-ps2.sh
@@ -53,10 +53,10 @@ strip = '${EE_BIN}/${PREFIX}-strip'
 pkgconfig = 'pkg-config'
 
 [built-in options]
-c_args = ['-D__PS2__', '-DPS2', '-march=r5900', '-mabi=eabi', '-mgp32', '-mfp32', '-G0', '-fno-exceptions', '-fno-rtti', '-fno-strict-aliasing', '-O2', '-DNDEBUG']
-cpp_args = ['-D__PS2__', '-DPS2', '-march=r5900', '-mabi=eabi', '-mgp32', '-mfp32', '-G0', '-fno-exceptions', '-fno-rtti', '-fno-strict-aliasing', '-std=c++17', '-O2', '-DNDEBUG']
-c_link_args = ['-march=r5900', '-mabi=eabi', '-mgp32', '-mfp32', '-G0']
-cpp_link_args = ['-march=r5900', '-mabi=eabi', '-mgp32', '-mfp32', '-G0']
+c_args = ['-D__PS2__', '-DPS2', '-march=r5900', '-mabi=eabi', '-mno-abicalls', '-fno-pic', '-mgp32', '-mfp32', '-G0', '-fno-exceptions', '-fno-rtti', '-fno-strict-aliasing', '-O2', '-DNDEBUG']
+cpp_args = ['-D__PS2__', '-DPS2', '-march=r5900', '-mabi=eabi', '-mno-abicalls', '-fno-pic', '-mgp32', '-mfp32', '-G0', '-fno-exceptions', '-fno-rtti', '-fno-strict-aliasing', '-std=c++17', '-O2', '-DNDEBUG']
+c_link_args = ['-march=r5900', '-mabi=eabi', '-mno-abicalls', '-fno-pic', '-mgp32', '-mfp32', '-G0']
+cpp_link_args = ['-march=r5900', '-mabi=eabi', '-mno-abicalls', '-fno-pic', '-mgp32', '-mfp32', '-G0']
 
 [properties]
 needs_exe_wrapper = true
