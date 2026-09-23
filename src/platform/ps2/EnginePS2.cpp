60e793ae8137f147f87d8693414df6ecefc4532e
Renan2010p <renanluscad@proton.me>
2026-09-23 19:56:35 -0300

    ps2: create working USB/Mass release package
    ISO creation has compatibility issues with PCSX2.
    Use USB/Mass storage method which is proven to work on PS2.
    
    Package contents:
    - fnwf.elf (16 MB)
    - SYSTEM.CNF (boot config)
    - assets/ (font, images, audio)
    
    Installation:
    1. Format USB in FAT32
    2. Create /fnwf/ folder
    3. Extract FNWF-PS2-Release.tar.gz
    4. Game auto-detects: mass:/fnwf/ or usbmass:/fnwf/

--- src/platform/ps2/EnginePS2.cpp
+++ src/platform/ps2/EnginePS2.cpp
@@ -57,7 +57,7 @@ std::string platform_path(std::string_view p) {
 
     // Known possible paths to try - prioritize USB mass storage
     static const char* s_paths[] = {
-        "cdrom:/fnwf/",          // CD-ROM (ISO)
+        // CD-ROM path commented out - using mass:/ instead
         "usbmass:/fnwf/",     // USB flash drive (most common)
         "mass:/fnwf/",        // Memory Stick
         "usbmass:/",          // USB root
